0d1b26601aec0177cf98395007bedbf30dd71493
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-06-25 19:35:35 +0200

    Fix example in doc

--- examples/simple_search.pl
+++ examples/simple_search.pl
@@ -15,15 +15,44 @@ my $obj = Tags::HTML::Search->new(
         'tags' => $tags,
 );
 
+# Initialize.
+$obj->init;
+
 # Process search.
 $obj->process_css;
 $obj->process;
 
 # Print out.
 print "CSS\n";
-print $css->flush."\n";
+print $css->flush."\n\n";
 print "HTML\n";
 print $tags->flush."\n";
 
 # Output:
-# TODO
+# CSS
+# .container {
+#         display: flex;
+#         align-items: center;
+#         justify-content: center;
+#         height: 100vh;
+# }
+# .form input {
+#         border-radius: 5px;
+# }
+#
+# HTML
+# <div class="container">
+#   <div class="inner">
+#     <form class="form" method="get">
+#       <p>
+#         <input autofocus="autofocus" type="text">
+#         </input>
+#       </p>
+#       <p>
+#         <button type="submit">
+#           Search
+#         </button>
+#       </p>
+#     </form>
+#   </div>
+# </div>
